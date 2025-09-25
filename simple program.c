f71711ee2d25f8fd65a5ebe4aae2ae2d1325cad0
eldad-00 <karanimwaura@gmail.com>
2025-09-26 00:11:16 +0300

    Add files via upload
    Simple program

---
+++ simple program.c
@@ -0,0 +1,16 @@
+//simple C program (Hellow world )
+/*
+Name; Eldad Mwaura 
+Reg no;CT101/G/26651/25
+Date;25.9.2025
+Description;Hellow world 
+*/
+
+
+//main function 
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
